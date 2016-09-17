6b029014da42167beff52e212504f06d7a4adfe3
gururajbn <n.gururajbn@gmail.com>
2016-09-17 13:22:16 +0530

    aded intial model schema

--- lazy/models.py
+++ lazy/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class lazy_links(models.Model):
+    user= models.ForeignKey(User)
+    created= models.DateTimeField(auto_now=True)
+    key= models.CharField(max_length=150)
+
+    def __str__(self):
+        return "{}-{}".format(self.user.username, self.created)
+
+    class Meta:
+        ordering = ['created']
